d27c9feb146b41fe20d75e021ec8c8272faaf968
unknown <bmurill2019@gmail.com>
2023-01-29 12:44:08 -0600

    Added myname.py
    this file complies with the assignment1

---
+++ brayanmurillogutierrez.py
@@ -0,0 +1,2 @@
+# this script prints my name
+print("Hello, my name is still here: Brayan Murillo Gutierrez")
--- test.md
+++ test.md
@@ -1,2 +1,4 @@
 # Hello, this is a test
 some description 2
+
+Watch tutorial on YouTube
